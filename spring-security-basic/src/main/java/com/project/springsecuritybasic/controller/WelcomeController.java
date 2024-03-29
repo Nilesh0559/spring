4b38222849b4dd233fcc9ae386990f422ec1ac57
Nilesh0559 <nileshgajendragadkar@gmail.com>
2024-03-29 19:53:41 +0530

    spring security basic with security starter dependency only

---
+++ spring-security-basic/src/main/java/com/project/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.project.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to spring with security";
+    }
+}
